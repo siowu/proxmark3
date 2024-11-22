dfc2dcb7d2acaf9a18cfe00e783ed8af26d564c9
西瓜味的奥利奥 <78242842+siowu@users.noreply.github.com>
2024-11-23 03:28:34 +0800

    Create script.js
    Signed-off-by: 西瓜味的奥利奥 <78242842+siowu@users.noreply.github.com>

---
+++ bootrom/script.js
@@ -0,0 +1,29 @@
+    var targetClass = Java.use('com.soft.blued.ui.user.adapter.UserInfoPrivateAlbumAdapter');
+    var BluedAlbum = Java.use('com.blued.android.module.common.user.model.BluedAlbum');
+
+    targetClass.a.overload('com.chad.library.adapter.base.BaseViewHolder', 'com.blued.android.module.common.user.model.BluedAlbum').implementation = function (baseViewHolder, bluedAlbum) {
+        var applyStatusField = BluedAlbum.class.getDeclaredField('applyStatus');
+        applyStatusField.setAccessible(true);
+        var originalValue = applyStatusField.getInt(bluedAlbum);
+        if (originalValue == 1) {
+            applyStatusField.setInt(bluedAlbum, 0);
+            this.a(baseViewHolder, bluedAlbum);
+            Java.scheduleOnMainThread(function () {
+                var Toast = Java.use('android.widget.Toast');
+                var context = Java.use('android.app.ActivityThread').currentApplication().getApplicationContext();
+                Toast.makeText(context, Java.use('java.lang.String').$new("偷看到咯~"), Toast.LENGTH_SHORT.value).show();
+            });
+        }
+        this.a(baseViewHolder, bluedAlbum);
+    };
+
+    var MsgChattingFragment = Java.use('com.soft.blued.ui.msg.MsgChattingFragment');
+    MsgChattingFragment.a.overload('com.soft.blued.model.UserInfoBasicModel').implementation = function (userInfoBasicModel) {
+        if (userInfoBasicModel.is_hide_distance.value == 1 || userInfoBasicModel.is_hide_last_operate.value == 1) {
+            var Toast = Java.use('android.widget.Toast');
+            var context = Java.use('android.app.ActivityThread').currentApplication().getApplicationContext();
+            var content = "距离:" + userInfoBasicModel.distance.value + "km\n在线时间:" + calTime(userInfoBasicModel.last_operate.value);
+            Toast.makeText(context, Java.use('java.lang.String').$new(content), Toast.LENGTH_LONG.value).show();
+        }
+        this.a(userInfoBasicModel);
+    };
